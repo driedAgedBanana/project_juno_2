fdb480e25607cb91f7a3e96ae537ae7642192eb9
driedAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-08-05 23:38:39 +0200

    Adding movement script
    I hate the new input system forever

--- Project_Juno_2/Assets/_script/Player/PlayerController.cs
+++ Project_Juno_2/Assets/_script/Player/PlayerController.cs
@@ -3,9 +3,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance;
+
     public Rigidbody rb;
     public float moveSpeed = 5f;
-    public InputAction playerAction;
+
+    private Vector2 _moveInput;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(Instance);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +33,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.Self);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _moveInput = ctx.ReadValue<Vector2>();
     }
 }
