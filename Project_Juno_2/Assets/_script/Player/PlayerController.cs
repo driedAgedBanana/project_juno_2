2d6040df48a440ac189040b808354f0c364ffb46
driedAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-08-05 23:15:31 +0200

    Adding weapon

---
+++ Project_Juno_2/Assets/_script/Player/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed = 5f;
+    public InputAction playerAction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
