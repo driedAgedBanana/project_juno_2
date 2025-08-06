f8afbad1c5f9905711365864acb0fbfeac511038
driedAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-08-06 15:32:37 +0200

    Adding handle cam look movement

--- Project_Juno_2/Assets/_script/Player/PlayerController.cs
+++ Project_Juno_2/Assets/_script/Player/PlayerController.cs
@@ -4,11 +4,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
-
-    public Rigidbody rb;
-    public float moveSpeed = 5f;
-
     private Vector2 _moveInput;
+
+    private bool _isAlive = true;
+    public Rigidbody rb;
+
+    [Header("Camera Settings")]
+    public Transform camHolder;
+    public float sensitivity = 15f;
+    private Vector2 _lookDirection;
+    [Range(0f, 90f)] private float _xClamp = 50f;
+    private float _xRotation;
+
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    [SerializeField] private bool _isMoving;
+
+
 
     private void Awake()
     {
@@ -22,23 +34,51 @@ public class PlayerController : MonoBehaviour
         }
 
         rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
-        transform.Translate(move * moveSpeed * Time.deltaTime, Space.Self);
+        if(_isAlive)
+        {
+            HandleLook();
+            HandleMovement();
+        }
+    }
+
+    private void HandleLook()
+    {
+        float mouseX = _lookDirection.x * sensitivity * Time.deltaTime;
+        float mouseY = _lookDirection.y * sensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+        _xRotation = Mathf.Clamp(_xRotation - mouseY, -_xClamp, _xClamp);
+        camHolder.localRotation = Quaternion.Slerp(camHolder.localRotation, Quaternion.Euler(_xRotation, 0f, 0f), Time.deltaTime * 10f);
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 movement = transform.right * _moveInput.x + transform.forward * _moveInput.y;
+        rb.linearVelocity = new Vector3(movement.x * moveSpeed, rb.linearVelocity.y, movement.z * moveSpeed);
+        _isMoving = _moveInput.magnitude > 0.01f;
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
         _moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext ctx)
+    {
+        _lookDirection = ctx.ReadValue<Vector2>();
     }
 }
