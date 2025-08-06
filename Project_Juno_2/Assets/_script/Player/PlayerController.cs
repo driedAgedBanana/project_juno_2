98d6ddc53d2fea64fbcaa7c6cc1f7d0ee95a6211
driedAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-08-06 16:27:41 +0200

    Adding running, slow walk and stamina mechanic.

--- Project_Juno_2/Assets/_script/Player/PlayerController.cs
+++ Project_Juno_2/Assets/_script/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,10 +17,37 @@ public class PlayerController : MonoBehaviour
     [Range(0f, 90f)] private float _xClamp = 50f;
     private float _xRotation;
 
-    [Header("Movement")]
+    [Header("Movement Mechanic")]
     public float moveSpeed = 5f;
+    public float slowWalkSpeed = 2f;
+    public float runSpeed = 10f;
+
+    // Stamina for running
+    public float currentStamina;
+    public float maxStamina;
+    public float staminaDrainRate;
+    public float staminaGainRate;
+    public float staminaFastGainRate;
+    public float coolDownTime;
+
+    private float _coolDownTimer;
+
+    [SerializeField] private bool _isSlowWalk;
+    [SerializeField] private bool _isRunning;
+    [SerializeField] private bool _runButtonPressed;
     [SerializeField] private bool _isMoving;
 
+    [Header("Crouching Mechanic (in development, not working right now)")]
+    public bool isCrouching;
+    public float crouchSpeed = 2f;
+
+    [Header("Leaning Mechanic")]
+    public float leaningAmount = 20f;
+    public float leaningSpeed = 15f;
+
+    private bool _canLean;
+    private Quaternion _targetLeanRotation;
+    private float _leaningDirection;
 
 
     private void Awake()
@@ -42,16 +70,19 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        _isAlive = true;
+        currentStamina = maxStamina;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_isAlive)
+        if (_isAlive)
         {
             HandleLook();
             HandleMovement();
+            HandleLeaning();
+            RegenerateStamina();
         }
     }
 
@@ -67,9 +98,64 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
+        bool canRun = _runButtonPressed && _isMoving && !isCrouching && currentStamina > 0;
+        float speed = isCrouching ? crouchSpeed : (canRun ? runSpeed : (_isSlowWalk ? slowWalkSpeed : moveSpeed));
+
         Vector3 movement = transform.right * _moveInput.x + transform.forward * _moveInput.y;
-        rb.linearVelocity = new Vector3(movement.x * moveSpeed, rb.linearVelocity.y, movement.z * moveSpeed);
+        rb.linearVelocity = new Vector3(movement.x * speed, rb.linearVelocity.y, movement.z * speed);
         _isMoving = _moveInput.magnitude > 0.01f;
+
+        if (canRun)
+        {
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+            _coolDownTimer = 0f;
+            _isRunning = true;
+            Debug.Log("Running!");
+        }
+        else
+        {
+            _isRunning = false;
+            Debug.Log("Or not...");
+        }
+
+        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+    }
+
+    private void RegenerateStamina()
+    {
+        if (currentStamina < maxStamina)
+        {
+            if (currentStamina == 0)
+            {
+                _coolDownTimer += Time.deltaTime;
+                if (_coolDownTimer < coolDownTime) return;
+            }
+
+            float regenerateStaminaRate = _isMoving ? staminaGainRate : staminaFastGainRate;
+
+            currentStamina += regenerateStaminaRate * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+        }
+    }
+
+    private void HandleLeaning()
+    {
+        _canLean = true;
+        if (_leaningDirection > 0) // lean right
+        {
+            _targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, -leaningAmount);
+        }
+        else if (_leaningDirection < 0) // lean left
+        {
+            _targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, leaningAmount);
+        }
+        else
+        {
+            _targetLeanRotation = Quaternion.Euler(0, transform.localEulerAngles.y, 0);
+        }
+
+        // smoothly transition to the target lean rotation
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, _targetLeanRotation, Time.deltaTime * leaningSpeed);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -80,5 +166,35 @@ public class PlayerController : MonoBehaviour
     public void OnLook(InputAction.CallbackContext ctx)
     {
         _lookDirection = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnLean(InputAction.CallbackContext ctx)
+    {
+        float leaningInput = ctx.ReadValue<float>();
+        _leaningDirection = leaningInput;
+    }
+
+    public void OnRun(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed)
+        {
+            _runButtonPressed = true;
+        }
+        if(ctx.canceled)
+        {
+            _runButtonPressed = false;
+        }
+    }
+
+    public void OnSlowWalk(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed)
+        {
+            _isSlowWalk = true;
+        }
+        if(ctx.canceled)
+        {
+            _isSlowWalk = false;
+        }
     }
 }
